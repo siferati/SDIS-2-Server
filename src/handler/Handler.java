5514748e4499a3a5885964b158916817dbb3f9ec
RuiCS <rui.pedro.csoares@gmail.com>
2017-05-13 14:25:46 +0100

    cleaner

--- src/handler/Handler.java
+++ src/handler/Handler.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 
+import java.util.HashMap;
+
 import java.sql.Connection;
 
 public class Handler implements HttpHandler {
@@ -35,5 +37,22 @@ public class Handler implements HttpHandler {
         os.close();
 
     } 
+
+    protected HashMap<String, String> getGETparams(HttpExchange t){
+        // Get Request Query
+        String query = t.getRequestURI().getQuery();
+
+        HashMap<String, String> queryPairs = new HashMap<String, String>();
+
+        if (query != null){
+            String[] params = query.split(this.AND_DELIMITER);
+
+            for (String param:params){
+                String[] paramPair = param.split(this.EQUAL_DELIMITER);
+                queryPairs.put(paramPair[0], paramPair[1]);
+            }
+        }
+        return queryPairs;
+    }
 
 }
--- src/handler/MapHandler.java
+++ src/handler/MapHandler.java
@@ -41,19 +41,7 @@ public class MapHandler extends Handler {
 
         System.out.println("GET " + t.getRequestURI());
 
-        // Get Request Query
-        String query = t.getRequestURI().getQuery();
-
-        HashMap<String, String> queryPairs = new HashMap<String, String>();
-
-        if (query != null){
-            String[] params = query.split(this.AND_DELIMITER);
-
-            for (String param:params){
-                String[] paramPair = param.split(this.EQUAL_DELIMITER);
-                queryPairs.put(paramPair[0], paramPair[1]);
-            }
-        }
+        HashMap<String, String> GETparams = this.getGETparams(t);
 
         // selects, wtv
     }

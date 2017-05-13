b442686d66dc9e5cf40293de4f5e9897bccea45d
RuiCS <rui.pedro.csoares@gmail.com>
2017-05-13 16:27:05 +0100

    Started Database

---
+++ db/maps.sql
@@ -0,0 +1,31 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS UserAcc;
+DROP TABLE IF EXISTS Map;
+DROP TABLE IF EXISTS MapLine;
+
+CREATE TABLE UserAcc (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    pass_hash TEXT NOT NULL,
+    email TEXT NOT NULL,
+
+);
+
+CREATE TABLE Map (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    dungeon_master INTEGER REFERENCES UserAcc(id),
+    rating INTEGER
+
+);
+
+CREATE TABLE MapLine(
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    draw TEXT NOT NULL,
+    map_id INTEGER REFERENCES Map(id) ON DELETE CASCADE
+
+);

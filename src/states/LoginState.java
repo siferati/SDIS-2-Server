6f967c6d29d5cd08425e3e5540837f25024a03ac
spininador <up201405079@fe.up.pt>
2017-05-26 12:12:50 +0100

    added states

---
+++ src/states/LoginState.java
@@ -0,0 +1,35 @@
+package states;
+
+import com.sun.net.httpserver.HttpExchange;
+import org.json.*;
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+import db.*;
+
+public class LoginState{
+	public String username;
+	public String userhash;
+	public String loginToken;
+
+	public LoginState(String username,String userhash){
+		//Assign username
+		this.username = username;
+		//Assign password
+		this.userhash = userhash;
+		//Generate random token
+		this.loginToken = LoginState.generateToken();
+	}
+
+	public static String generateToken(){
+		char[] chars = "abcdefghijklmnopqrstuvwxyz1234567890".toCharArray();
+		StringBuilder sb = new StringBuilder();
+		Random random = new Random();
+		for (int i = 0; i < 16; i++) {
+		    char c = chars[random.nextInt(chars.length)];
+		    sb.append(c);
+		}
+		String output = sb.toString();
+		return output;
+	}
+}
---
+++ src/states/States.java
@@ -0,0 +1,12 @@
+package states;
+
+import com.sun.net.httpserver.HttpExchange;
+import org.json.*;
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+import db.*;
+
+public class States{
+	public static ArrayList<LoginState> loggedUsers;
+}
